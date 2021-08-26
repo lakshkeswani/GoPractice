9170b63871745880e18917f28224c2de64c2ad8d
laksh58 <[Your>
2021-08-26 15:34:37 +0500

    remove spaces completed and continue is used

--- exploreVariables.go
+++ exploreVariables.go
@@ -9,15 +9,17 @@ import (
 
 //global variable
 var (
-	name string ="laksh"
-    age int = 21
-
+	name string = "laksh"
+	age  int    = 21
 )
+
 // Rules of variable names
 // lowercase variables are for within package uppercase will be expose to other packages
 // legnth of variable is equal to life of variable
 //keep the acronyms
 func main() {
+	fmt.Println("2D Array")
+	loops.MultidimenssionalArray()
 	fmt.Println("remove spaces")
 	loops.Removespace()
 	fmt.Println("Sum of Given digit")
@@ -29,12 +31,12 @@ func main() {
 	//blocked scope
 	println("Output of Explore variable")
 
-	var theURL ="https://google.com"
-	println("url =",theURL)
-	println("before decleration in main ",name,"age = ",age)
+	var theURL = "https://google.com"
+	println("url =", theURL)
+	println("before decleration in main ", name, "age = ", age)
 	// name local variable age is global
-var name string ="laksh kumar"
+	var name string = "laksh kumar"
 
-	println("after decleration in main ",name,"age = ",age)
-//this is called shadowing go will use the inner most scope
+	println("after decleration in main ", name, "age = ", age)
+	//this is called shadowing go will use the inner most scope
 }
