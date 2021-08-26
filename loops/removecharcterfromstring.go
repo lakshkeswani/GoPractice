3027c99401d13acba4c4dcde985a9d9de4e21d2f
laksh58 <[Your>
2021-08-26 14:27:37 +0500

    remove spaces completed and continue is used

--- loops/removecharcterfromstring.go
+++ loops/removecharcterfromstring.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	_ "fmt"
 )
-func Removespace() {
-	str:="hello how are you?"
-	for _,element:=range str{
-		cmp:=string(element)==" "
-	 if cmp{
-	 	
-	 }
-		fmt.Println("element ",string(element))
-		}
-	}
 
+func Removespace() {
+	str := "hello how are you?"
+	for _, element := range str {
+		cmp := string(element) == " "
+		if cmp {
+			continue
+		}
+		fmt.Printf("%s", string(element))
+	}
+}
